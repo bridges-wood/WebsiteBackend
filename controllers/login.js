8c88c657f0f5070e5c6edf7e1e9ff2e40f8e3b4a
Max Wood <bridges.wood@gmail.com>
2020-05-22 18:21:36 +0100

    Addition of token verification.

--- controllers/login.js
+++ controllers/login.js
@@ -8,27 +8,45 @@ const loginRouter = express.Router()
 loginRouter.post('/', async (req, res) => {
 	const { body } = req
 
-	const user = await User.findOne({ username: body.username })
-	const passwordCorrect = user === null
-		? false
-		: await bcrypt.compare(body.password, user.passwordHash)
+	if (!body.verify) 
+	{
+		const user = await User.findOne({ username: body.username })
+		const passwordCorrect = user === null
+			? false
+			: await bcrypt.compare(body.password, user.passwordHash)
 	
-	if ( ! ( user && passwordCorrect ) ) {
-		res.status(401).json({
-			error: 'invalid username or password'
-		})
+		if ( ! ( user && passwordCorrect ) ) {
+			res.status(401).json({
+				error: 'invalid username or password'
+			})
+		}
+
+		const userForToken = {
+			username: user.username,
+			id: user._id
+		}
+
+		const token = jwt.sign(userForToken, process.env.SECRET)
+
+		res
+			.status(200)
+			.send({ token, username: user.username, name: user.name })
+	} else {
+		try {
+			const decoded = jwt.verify(body.token, process.env.SECRET)
+			const unixTime = Math.round(new Date().getTime() / 1000)
+			if ( (unixTime - decoded.iat) > 3600 ) {
+				res.status(401).json({
+					error: 'token timed out'
+				})
+			}
+			res.status(200).send(body)
+		} catch (error) {
+			res.status(401).json({
+				error: 'invalid session token'
+			})
+		}
 	}
-
-	const userForToken = {
-		username: user.username,
-		id: user._id
-	}
-
-	const token = jwt.sign(userForToken, process.env.SECRET)
-
-	res
-		.status(200)
-		.send({ token, username: user.username, name: user.name })
 })
 
 export default loginRouter
