dcfe689cafcb53c737fac89df773f894f3cdbd9c
Max Wood <bridges.wood@gmail.com>
2020-06-07 14:03:11 +0100

    uibuild

---
+++ build/precache-manifest.9ea8e924b163c5f325ff237a887dd85d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da0d046190aff1ad0ece8e4f3c255af2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8c3e97caed6f916a4cda",
+    "url": "/static/css/main.d8732b50.chunk.css"
+  },
+  {
+    "revision": "63cdd867bf276ccd63a8",
+    "url": "/static/js/2.9522061e.chunk.js"
+  },
+  {
+    "revision": "8ebb70227fec3b61ad3af3d99ebe8940",
+    "url": "/static/js/2.9522061e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8c3e97caed6f916a4cda",
+    "url": "/static/js/main.f99b6ae1.chunk.js"
+  },
+  {
+    "revision": "42dded47c92ef7027de7",
+    "url": "/static/js/runtime-main.f2c5987a.js"
+  }
+]);
