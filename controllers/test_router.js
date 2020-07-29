a38aa0d6f76aac80a81f2846dfa05bc31ac93ec0
Max Wood <bridges.wood@gmail.com>
2020-07-29 15:19:51 +0100

    Addition of post router

--- controllers/test_router.js
+++ controllers/test_router.js
@@ -11,4 +11,13 @@ router.post('/reset', async (req, res) => {
 	res.status(204).end()
 })
 
+router.post('/projects', async (req, res) => {
+	const project = req.body
+	const formatted = new Project({...project})
+
+	await formatted.save()
+
+	res.status(200).end()
+})
+
 export default router
